775c6302c93bd98e60ee827a0402246287abd1e8
bhupendra <bhupendraverma7388@gmail.com>
2023-07-26 16:18:27 +0530

    commiting 2 ExceptionThrows.java

---
+++ ExceptionThrows.java
@@ -0,0 +1,8 @@
+import java.io.FileInputStream;
+
+public class ExceptionThrows{
+    public static void main(String[] args) throws Exception  {
+
+        FileInputStream f=new FileInputStream("D:/abcd.txt");
+    }
+}
